8ef0420cd9d119380f7344031e5b2b812e20e60b
lsewilson <laurawilson@hotmail.co.uk>
2016-11-20 17:13:46 +0000

    'schedule a viewing' contains text and tests passing

--- components.js
+++ components.js
@@ -3,7 +3,9 @@ import React, { PropTypes } from 'react';
 class ScheduleButton extends React.Component {
   render() {
     return (
-      <div className="schedule-button" />
+      <div className="schedule-button">
+      <p id="text">SCHEDULE A VIEWING!</p>
+      </div>
     );
   }
 }
--- test/schedule-button-test.js
+++ test/schedule-button-test.js
@@ -5,7 +5,20 @@ import ScheduleButton from '../components.js'
 
 
 describe('ScheduleButton Component', function() {
+
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = shallow(<ScheduleButton />);
+  });
+
   it ('should have a class named schedule-button', function() {
-    expect(shallow(<ScheduleButton />).is('.schedule-button')).to.equal(true);
+    expect(wrapper.is('.schedule-button')).to.equal(true);
+  })
+  it ('should have one child element', function() {
+    expect(wrapper.children().length).to.equal(1);
+  })
+  it ('should contain text "SCHEDULE A VIEWING!"', function() {
+    expect(wrapper.text()).to.equal('SCHEDULE A VIEWING!');
   })
 })
