c45c973b249c68f1ae3e32f4781670be79b4d806
lsewilson <laurawilson@hotmail.co.uk>
2016-11-21 11:30:33 +0000

    corrected source for minimize icon image

--- src/minimize-box.js
+++ src/minimize-box.js
@@ -4,7 +4,7 @@ class MinimizeBox extends React.Component {
   render() {
     return (
       <div className="minimize-box">
-        <img src="img/arrow.png"/>
+        <img src={require("./img/arrow.png")}/>
       </div>
     );
   }
