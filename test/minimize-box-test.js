e8e2282729b046b2cd62db39bd82c23ae16da44a
lsewilson <laurawilson@hotmail.co.uk>
2016-11-23 22:15:56 +0000

    minimize box tests with use of mockery

--- test/minimize-box-test.js
+++ test/minimize-box-test.js
@@ -2,35 +2,38 @@ import React from 'react';
 import { expect } from 'chai';
 import sinon from 'sinon';
 import { shallow, mount, render } from 'enzyme';
+import mockery from "mockery";
 import MinimizeBox from '../src/components/minimize-box'
 
+mockery.enable();
+mockery.registerMock('../img/arrow.png', 0)
 
-// describe('MinimizeBox Component', function() {
-//
-//   let wrapper;
-//
-//   beforeEach(() => {
-//     wrapper = shallow(<MinimizeBox />);
-//   });
-//
-//   it ('should have a class named minimize-box', function() {
-//     expect(wrapper.is('.minimize-box')).to.equal(true);
-//   });
-//
-//   it ('should have one child element', function() {
-//     expect(wrapper.children().length).to.equal(1);
-//   });
-//
-//   it ('should have a child element which is an image', function() {
-//     const image = wrapper.find('img');
-//     expect(image.prop('src')).to.equal('./img/arrow.png');
-//   });
-//
-//   it ('is a clickable button', function(){
-//     const handleClick = sinon.spy()
-//     const mount_wrapper = mount(<MinimizeBox onClick={handleClick}/>)
-//     mount_wrapper.find('.minimize-box').simulate('click');
-//     expect(handleClick).to.have.property('callCount', 1);
-//   })
-//
-// });
+describe('MinimizeBox Component', function() {
+
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = shallow(<MinimizeBox />);
+  });
+
+  it ('should have a class named minimize-box', function() {
+    expect(wrapper.is('.minimize-box')).to.equal(true);
+  });
+
+  it ('should have one child element', function() {
+    expect(wrapper.children().length).to.equal(1);
+  });
+
+  it ('should have a child element which is an image', function() {
+    const image = wrapper.find('img');
+    expect(wrapper.find('img')).to.exist;
+  });
+
+  it ('is a clickable button', function(){
+    const handleClick = sinon.spy()
+    const mount_wrapper = mount(<MinimizeBox onClick={handleClick}/>)
+    mount_wrapper.find('.minimize-box').simulate('click');
+    expect(handleClick).to.have.property('callCount', 1);
+  })
+
+});
