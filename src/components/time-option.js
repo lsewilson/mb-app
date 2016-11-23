b97867641b9b151f1a181747961372c6593680ae
lsewilson <laurawilson@hotmail.co.uk>
2016-11-23 20:00:05 +0000

    time options can be unselected

--- src/components/time-option.js
+++ src/components/time-option.js
@@ -8,7 +8,18 @@ class TimeOption extends React.Component {
   };
 
   handleClick() {
-    this.props.bookings.push({date: this.props.currentDay, time: this.props.text})
+    let option = {date: this.props.currentDay, time: this.props.text};
+    if (this.props.bookings.length === 0 ) {
+      this.props.bookings.push(option);
+    } else {
+      this.props.bookings.forEach(function(booking, i, arr) {
+        if (option.date === booking.date && option.time === booking.time) {
+          arr.splice(i, 1);
+        } else {
+          arr.push(option);
+        }
+      });
+    }
   }
 
   render() {
