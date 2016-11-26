00bcf4fa3c3b499a920ed04290646b04bffc16bf
lsewilson <laurawilson@hotmail.co.uk>
2016-11-26 14:37:47 +0000

    fixes time selection bug

--- src/components/time-option.js
+++ src/components/time-option.js
@@ -12,13 +12,17 @@ class TimeOption extends React.Component {
     if (this.props.bookings.length === 0 ) {
       this.props.bookings.push(option);
     } else {
-      this.props.bookings.forEach(function(booking, i, arr) {
-        if (option.date === booking.date && option.time === booking.time) {
-          arr.splice(i, 1);
-        } else {
-          arr.push(option);
-        }
-      });
+      this.checkExisting(0,this.props.bookings,option)
+    }
+  }
+
+  checkExisting(i, array, option) {
+    if (array[i].date === option.date && array[i].time === option.time) {
+      array.splice(i,1);
+    } else if (i < array.length - 1) {
+        this.checkExisting(i+1, array, option);
+    } else {
+      array.push(option)
     }
   }
 
